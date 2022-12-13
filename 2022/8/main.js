293f21de157aa6f69096fb0661ff8eb9a196c38d
filomar03 <unacreaturadivina@gmail.com>
2022-12-13 21:09:19 +0100

    doing 8

--- 2022/8/main.js
+++ 2022/8/main.js
@@ -1,4 +1,11 @@
 const input = require('./input.js');
 const utils = require('./utils.js');
 
-console.log(input.data);
+let grid = input.data.map((row, rowIndex) => {
+    return row.map((height, columnIndex) => {
+        let isVisible = rowIndex == 0 || columnIndex == 0 || rowIndex == input.data.length - 1 || columnIndex == row.length - 1 ? true : null;
+        return new utils.Tree(height, isVisible);
+    })
+})
+
+console.log(grid);
---
+++ 2022/8/utils.js
@@ -0,0 +1,8 @@
+const Tree = function (height, isVisible) {
+    this.height = height;
+    this.isVisible = isVisible;
+}
+
+module.exports = {
+    Tree
+}
