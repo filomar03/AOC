eb36257bc5fad635b0ecc63ccff6c6e78b384532
filomar03 <unacreaturadivina@gmail.com>
2025-12-03 21:58:29 +0100

    Update main.rs
    un po di parte 2

--- 2025/day02/src/main.rs
+++ 2025/day02/src/main.rs
@@ -1,12 +1,3 @@
-use std::{
-    collections,
-    env::current_exe,
-    ops::{Add, AddAssign, Index},
-    slice::SliceIndex,
-    str::FromStr,
-    sync::mpsc::SendError,
-};
-
 use common::read_input;
 use itertools::Itertools;
 
@@ -21,44 +12,60 @@ fn main() {
             // => Index::index(self: &[T], idx: Index<SliceIndex<[T]>>)
             // => RangeFull -> impl -> SliceIndex<[T]>
             // => idx.index(self, &[T]) -> whole slice
-            if let [start, _, end] = r.split("-").take(3).collect::<Vec<&str>>()[..] {
+            if let [start, end] = r.split("-").take(3).collect::<Vec<&str>>()[..] {
                 Range::new(Id(start.parse().unwrap()), Id(end.parse().unwrap()))
             } else {
                 panic!("Bad formatting");
             }
         })
         .collect();
+
+    let mut acc: Vec<Id> = vec![];
+    for r in ranges {
+        let x = r.get_invalid();
+        let tmp: Vec<String> = x.iter().map(|f| f.0.to_string()).collect();
+        println!("start: {:?}\nend: {:?}\n{}", r.start, r.end, tmp.join(", "));
+
+        acc.extend(x);
+    }
 }
 
-struct Id(u32);
+#[derive(Clone, Copy, Debug)]
+struct Id(u64);
 
 impl Id {
     fn id_valid(&self) -> bool {
         let str = self.0.to_string();
-        str.len()
+        if str.len() % 2 != 0 {
+            return true;
+        }
+
+        let a = &str[..str.len()];
+        let b = &str[str.len()..];
+
+        a == b
     }
 }
 
 struct Range {
     start: Id,
     end: Id,
-    current: u32,
 }
 
 impl Range {
     fn new(start: Id, end: Id) -> Self {
-        let current = start.0;
-        Self {
-            start,
-            end,
-            current,
-        }
+        Self { start, end }
     }
 
-    fn get_invalid(&mut self) -> Vec<Id> {
+    fn get_invalid(&self) -> Vec<Id> {
         let mut ids = Vec::new();
-        while self.current <= self.end.0 {
-            ids.push(Id(self.current));
+        let mut current = self.start;
+        while current.0 <= self.end.0 {
+            if !current.id_valid() {
+                ids.push(current);
+            }
+
+            current.0 += 1;
         }
         ids
     }
