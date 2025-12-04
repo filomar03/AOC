5704dbd22a7cf43be55cfc916dbd05b4c13243a8
filomar03 <unacreaturadivina@gmail.com>
2025-12-04 20:01:34 +0100

    day03 p1

--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -1,22 +1,10 @@
-use std::{collections::VecDeque, f64::INFINITY};
-
 use common::read_input;
-
-type joltage = u32;
-type Bank = Vec<joltage>;
 
 fn main() {
     let input = read_input();
-    let banks: Vec<Bank> = input
-        .split("\n")
-        .map(|b| {
-            b.split("")
-                .map(|c| c.parse::<joltage>().unwrap())
-                .collect::<Bank>()
-        })
-        .collect();
+    let banks: Vec<BatteryBank> = Vec::from_iter(input.split("\n").map(|b| BatteryBank::new(b)));
 
-    for bank in banks {
+    for bank in &banks {
         println!(
             "{} -> {}",
             bank.iter()
@@ -26,15 +14,45 @@ fn main() {
             get_max_joltage(bank)
         );
     }
+
+    // let acc = banks.iter().fold(0, |acc, b| acc + get_max_joltage(b));
+    // println!("Answer (part 1): {}", acc);
 }
 
-fn get_max_joltage(bank: Bank) -> joltage {
-    let mut max_j = 0;
-    for i in 0..bank.len() - 1 {
-        let j = bank[i] * 10 + bank[i + 1];
-        if j > max_j {
-            max_j = j;
+const BASE: u32 = 10;
+
+type joltage = u32;
+
+struct BatteryBank {
+    bank: Vec<joltage>,
+}
+
+impl BatteryBank {
+    fn new(bank: &str) -> Self {
+        BatteryBank {
+            bank: Vec::from_iter(bank.chars().map(|c| c.to_digit(BASE).unwrap())),
         }
     }
-    max_j
+
+    fn max_joltage(&self, battery_num: u32) -> joltage {
+        let batteries: &[joltage] = Box::new([0, battery_num]);
+    }
 }
+
+// fn get_max_joltage(bank: &Bank) -> joltage {
+//     let mut j_d1 = bank[0];
+//     let mut j_d2 = bank[1];
+//     for i in 1..bank.len() - 1 {
+//         let a = bank[i];
+//         let b = bank[i + 1];
+
+//         if bank[i] > j_d1 {
+//             j_d1 = bank[i];
+//             j_d2 = bank[i + 1];
+//         } else if bank[i + 1] > j_d2 {
+//             j_d2 = bank[i + 1];
+//         }
+//     }
+
+//     j_d1 * 10 + j_d2
+// }
